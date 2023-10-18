6740aec2d56354af3fe8c568d0c6b6ee082b62d4
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-10-18 23:48:53 +0900

    🎨 table_generatorのクラスを削除

--- main/example/tools/table_generator.py
+++ main/example/tools/table_generator.py
@@ -2,6 +2,5 @@ import pandas as pd
 from tabulate import tabulate
 
 
-class TableCreator:
-    def display(self, df: pd.DataFrame):
-        print(tabulate(df, headers="keys", tablefmt="psql"))
+def display(df: pd.DataFrame):
+    print(tabulate(df, headers="keys", tablefmt="psql"))
